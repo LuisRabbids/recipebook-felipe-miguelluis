486a3a924487ec949c1417dfa9f6b4b597edd13e
Luis Felipe <luisfelipecodes@gmail.com>
2025-03-01 21:10:51 +0800

    Fixed the get absolute url method of Ingredient

--- ledger/models.py
+++ ledger/models.py
@@ -9,7 +9,7 @@ class Ingredient(models.Model):
         return self.name
     
     def get_absolute_url(self):
-        return reverse('recipe_detail', args=[str(self.id)])
+        return reverse('ingredient_detail', args=[str(self.id)])
 
 
 class Recipe(models.Model):
