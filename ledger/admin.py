ed17280d15ddc7f8c2f19a0e969c64582d1e4721
Luis Felipe <luisfelipecodes@gmail.com>
2025-03-01 22:16:02 +0800

    Adjusted gitignore

--- ledger/admin.py
+++ ledger/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from .models import Recipe, Ingredient, RecipeIngredient
 
 
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+
+
 admin.site.register(Recipe)
 admin.site.register(Ingredient)
 admin.site.register(RecipeIngredient)
