483421c242da971b2550e32c9020d8614c92282e
Luis Felipe <luisfelipecodes@gmail.com>
2025-03-01 21:42:50 +0800

    Added the recipe_detail() view

--- ledger/views.py
+++ ledger/views.py
@@ -10,35 +10,6 @@ def recipes_lists(request):
     return render(request, "ledger/recipes_list.html", ctx)
 
 
-def first_recipe(request):
-    ctx = {
-        "name": "Recipe 1",
-        "ingredients": [
-            {"name": "tomato", "quantity": "3pcs"},
-            {"name": "onion", "quantity": "1pc"},
-            {"name": "pork", "quantity": "1kg"},
-            {"name": "water", "quantity": "1L"},
-            {"name": "sinigang mix", "quantity": "1 packet"}
-        ],
-        "link": "/recipe/1"
-    }
-
-    return render(request, "ledger/first_recipe.html", ctx)
-
-
-def second_recipe(request):
-    ctx = {
-        "name": "Recipe 2",
-        "ingredients": [
-            {"name": "garlic", "quantity": "1 head"},
-            {"name": "onion", "quantity": "1pc"},
-            {"name": "vinegar", "quantity": "1/2cup"},
-            {"name": "water", "quantity": "1 cup"},
-            {"name": "salt", "quantity": "1 tablespoon"},
-            {"name": "whole black peppers", "quantity": "1 tablespoon"},
-            {"name": "pork", "quantity": "1 kilo"}
-        ],
-        "link": "/recipe/2"
-    }
-
-    return render(request, "ledger/second_recipe.html", ctx)
+def recipe_detail(request, id):
+    ctx = {'recipe': Recipe.objects.get(id=id)}
+    return render(request, 'ledger/recipe_detail.html', ctx)
