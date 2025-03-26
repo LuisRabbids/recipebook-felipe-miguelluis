0309139138dfcf69efd2174b35539fdd4841f8d3
Luis Felipe <luisfelipecodes@gmail.com>
2025-03-26 14:05:39 +0800

    Modified Recipe() class to have created_on and updated_on fields

--- ledger/models.py
+++ ledger/models.py
@@ -16,6 +16,8 @@ class Ingredient(models.Model):
         Profile,
         on_delete=models.CASCADE
     )
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
