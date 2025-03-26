433531174d63057cc10d8413c1fa8ba209ddaff6
Luis Felipe <luisfelipecodes@gmail.com>
2025-03-26 15:55:05 +0800

    Fixed Recipe() class' author to a CharField

--- ledger/models.py
+++ ledger/models.py
@@ -21,10 +21,7 @@ class Ingredient(models.Model):
 
 class Recipe(models.Model):
     name = models.CharField(max_length=50)
-    author = models.ForeignKey(
-        Profile,
-        on_delete=models.CASCADE
-    )
+    author = models.CharField(max_length=50)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
