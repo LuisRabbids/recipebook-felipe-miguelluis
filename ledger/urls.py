25943ce53c04d224e130e6798bef82d9fff91a06
Luis Felipe <luisfelipecodes@gmail.com>
2025-03-31 23:11:46 +0800

    Added url for the recipe adding page

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import recipes_lists, recipe_detail
+from .views import recipes_lists, recipe_detail, add_recipe
 
 urlpatterns = [
     path('recipes/list', recipes_lists, name="recipes-lists"),
-    path("recipe/<int:id>/", recipe_detail, name="recipe_detail"),
+    path("recipe/<int:id>/", recipe_detail, name="recipe-detail"),
+    path("recipe/add/", add_recipe, name="add-recipe")
 ]
 
 app_name = 'ledger'
