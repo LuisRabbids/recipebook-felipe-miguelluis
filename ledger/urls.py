940bfe34cda8e5f98daa7268f125175821efd52c
Luis Felipe <luisfelipecodes@gmail.com>
2025-04-01 10:10:03 +0800

    Added add_recipe_image() view and changed all id occurrences to pk

--- ledger/urls.py
+++ ledger/urls.py
@@ -3,7 +3,7 @@ from .views import recipes_lists, recipe_detail, add_recipe
 
 urlpatterns = [
     path('recipes/list', recipes_lists, name="recipes-lists"),
-    path("recipe/<int:id>/", recipe_detail, name="recipe-detail"),
+    path("recipe/<int:pk>/", recipe_detail, name="recipe-detail"),
     path("recipe/add/", add_recipe, name="add-recipe")
 ]
 
--- ledger/views.py
+++ ledger/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Recipe
-from .forms import RecipeForm
+from .forms import RecipeForm, RecipeImageForm
 
 
 @login_required
@@ -14,21 +14,39 @@ def recipes_lists(request):
 
 
 @login_required
-def recipe_detail(request, id):
-    ctx = {'recipe': Recipe.objects.get(id=id)}
+def recipe_detail(request, pk):
+    ctx = {'recipe': Recipe.objects.get(pk=pk)}
     return render(request, 'ledger/recipe_detail.html', ctx)
 
 
 @login_required
 def add_recipe(request):
     form = RecipeForm()
+    
     if request.method == "POST":
         form = RecipeForm(request.POST)
         if form.is_valid():
             recipe = form.save()
             recipe.author = request.user
             recipe.save()
-            return redirect("ledger:recipe-detail", id=recipe.id)
+            return redirect("ledger:recipe-detail", pk=recipe.pk)
 
     ctx = {"form": form}
     return render(request, "ledger/add_recipe.html", ctx)
+
+
+@login_required
+def add_recipe_image(request, pk):
+    recipe = get_object_or_404(Recipe, pk=pk)
+    form = RecipeImageForm()
+
+    if request.method == "POST":
+        form = RecipeImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            image = form.save()
+            image.recipe = recipe  
+            image.save()
+            return redirect("ledger:recipe-detail", pk=recipe.pk)
+        
+    ctx = {"form": form, "recipe": recipe}
+    return render(request, "ledger/add_recipe_image.html", ctx)
