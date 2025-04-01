510a3111f02651d78c7d6bcb61abc26c7488467c
Luis Felipe <luisfelipecodes@gmail.com>
2025-04-01 10:01:20 +0800

    Added RecipeImageForm() to handle adding recipe images

--- ledger/forms.py
+++ ledger/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from .models import Recipe
+from .models import Recipe, RecipeImage
 
 
 class RecipeForm(forms.ModelForm):
     class Meta:
         model = Recipe
         fields = ["name", "description"]
+
+
+class RecipeImageForm(forms.ModelForm):
+    class Meta:
+        model = RecipeImage
+        fields = ["recipe_image", "description"]
